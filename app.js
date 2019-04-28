79113ceb98f86cb3e9251653fb904cd07e2e92f2
lionelbergen@live.com <lionelbergen@live.com>
2019-04-28 09:54:44 -0500

    changed app.js

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ let LeagueAPI = require('./LeagueAPI/LeagueAPI.js');
 LeagueAPI = new LeagueAPI(APIKeys.LeagueApiKey, Region.NA);
 
 // Example usage:
-LeagueAPI.getSummonerByName().then(function(data) {
+/*LeagueAPI.getSummonerByName('LeagueOfSausage').then(function(data) {
 	return LeagueAPI.getMatchList(data);
 })
 .then(console.log)
+.catch(console.log);*/
+
+var summonerObject;
+
+LeagueAPI.getSummonerByName('LeagueOfSausage').then(function(data) {
+	summonerObject = data;
+	return LeagueAPI.getActiveGames(data);
+})
+.then(console.log)
 .catch(console.log);
+
+
+/*Promise.all([LeagueAPI.getSummonerByName(), LeagueAPI.getMatchList()]).then(function(result) {
+	console.log(result);
+}).catch(console.log);
+*/
