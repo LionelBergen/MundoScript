21464037e6a840e576bad5c79063344c0a6440bf
lionelbergen@live.com <lionelbergen@live.com>
2019-05-09 18:23:03 -0500

    Removed unused classes

--- LeagueAPI/classes/index.js
+++ LeagueAPI/classes/index.js
@@ -1,7 +1,5 @@
 LeagueAccountInfo = require('./LeagueAccountInfo.js');
 Region = require('./Region.js');
-ChampionRotation = require('./ChampionRotation.js');
 MatchInfo = require('./MatchInfo.js');
-MatchParticipant = require('./MatchParticipant.js');
 
-module.exports = {LeagueAccountInfo, ChampionRotation, Region, MatchInfo, MatchParticipant}
+module.exports = {LeagueAccountInfo, Region, MatchInfo}
